5ea8d510006c8b6ae3a16d53c92f7c01358a68c0
ashince <ashincekd@gmail.com>
2018-07-06 23:01:18 +0530

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,8 @@
+ subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.9-rpms" \
+    --enable="rhel-7-fast-datapath-rpms" \
+    --enable="rhel-7-server-ansible-2.4-rpms"
+    
+yum install wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
